1a589d73375b6ba9f71c3430fbd9f932ae7bd7ac
isakilikya <isakilikya@gmail.com>
2020-12-08 20:23:30 +0300

    feat: JavaScript'e donus

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Logo from '../Logo';
+
+export default function Footer() {
+    return (
+        <footer className="App-footer">
+            <Logo />
+            <p>Footer</p>
+        </footer>
+    )
+}
