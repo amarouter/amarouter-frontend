84d8609a41a5662e79b83bd1ed602d5ec9d2ac20
isakilikya <isaimadoglu@gmail.com>
2021-02-27 20:00:39 +0300

    Sosyal medya logolari eklendi.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
+import { FaGithub, FaYoutube, FaLinkedin, FaTwitter } from "react-icons/fa";
+
 import Logo from "../Logo";
+import './Footer.css';
 
 export default function Footer() {
   return (
     <footer className="App-footer pt-3">
       <Container>
         <Row>
-          <Col>
+          <Col md={3}>
             <Logo />
           </Col>
           <Col md={3}>
@@ -26,8 +29,14 @@ export default function Footer() {
               <li>Bize Ulaşın</li>
             </ul>
           </Col>
-          <Col>
+          <Col md={3}>
             <h5>Bizi Takip Edin</h5>
+            <ul className="list-unstyled text-small horizontal-list">
+              <li><a href="https://github.com/amarouter" target="_blank"><FaGithub /></a></li>
+              <li><a href="https://www.youtube.com/channel/UC6179yTWBNkRatwNxntGMrQ" target="_blank"><FaYoutube /></a></li>
+              <li><a href="https://www.linkedin.com/in/amarouter/" target="_blank"><FaLinkedin /></a></li>
+              <li><a href="https://twitter.com/amarouterorg" target="_blank"><FaTwitter /></a></li>
+            </ul>
           </Col>
         </Row>
       </Container>
