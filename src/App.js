308019fea32f093c6967d02e226d048e0ca655bc
iremkilikya <iremkilikya@gmail.com>
2021-03-02 09:27:55 +0300

    react-router-dom eklendi.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Main from './components/main/Main';
-import Footer from './components/footer/Footer';
+import Main from "./components/main/Main";
+import Blog from "./components/blog/Blog";
+import Footer from "./components/footer/Footer";
 
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import NotFound from "./components/notfound/NotFound";
+
 
 function App() {
   return (
     <div className="App">
-      <Main />
-      {/* Blog */}
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/blog" component={Blog} />
+        <Route component={NotFound} />
+      </Switch>
       <Footer />
     </div>
   );
---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import "./NotFound.css";
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div className="not-found">
+                Not Found!
+            </div>
+        )
+    }
+}
