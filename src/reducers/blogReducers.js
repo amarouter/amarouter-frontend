7f951948fecec7bbec40def97fa4c3f08a47cb1d
isakilikya <isaimadoglu@gmail.com>
2021-05-02 20:49:50 +0300

    blogConstants eklendi.

--- src/reducers/blogReducers.js
+++ src/reducers/blogReducers.js
@@ -1,12 +1,18 @@
+import {
+  BLOG_LIST_REQUEST,
+  BLOG_LIST_SUCCESS,
+  BLOG_LIST_FAIL,
+} from "../constants/blogConstants";
+
 export const blogListReducers = (state = { blogs: [] }, action) => {
   switch (action.type) {
-    case "BLOG_LIST_REQUEST":
+    case BLOG_LIST_REQUEST:
       return { loading: true, blogs: [] };
 
-    case "BLOG_LIST_SUCCESS":
+    case BLOG_LIST_SUCCESS:
       return { loading: false, blogs: action.payload };
 
-    case "BLOG_LIST_FAIL":
+    case BLOG_LIST_FAIL:
       return { loading: false, error: action.payload };
 
     default:
