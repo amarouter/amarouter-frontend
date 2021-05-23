6b7109a88b1772af0dc6bc9843b89744a6688f14
isakilikya <isaimadoglu@gmail.com>
2021-05-23 07:33:41 +0300

    refactor: Main component ismi HomeScreen oldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Main from "./screens/Main";
+import HomeScreen from "./screens/HomeScreen";
 import Blog from "./screens/Blog";
 import BlogPost from "./screens/BlogPost";
 import Footer from "./components/Footer";
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={HomeScreen} />
         <Route exact path="/blog" component={Blog} />
         <Route path="/blog/:slug" component={BlogPost} />
         <Route component={NotFound} />
