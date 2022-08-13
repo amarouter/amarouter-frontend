1f5c5d93eb6da1088ec5e5bfd0677e8ff8f76539
irembuz <irembuz7@gmail.com>
2022-08-13 11:44:02 +0300

    BlogPost detail page article element layout

---
+++ src/components/BlogPostDetailArticle.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import ReactMarkdown from "react-markdown";
+
+import Loader from "./Loader";
+import Message from "./Message";
+
+const BlogPostDetailArticle = () => {
+  const blogPostSelector = useSelector((state) => state.blogPostDetails);
+  const { loading, error, blogPost } = blogPostSelector;
+
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <article>
+          <ReactMarkdown children={blogPost.text} />
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogPostDetailArticle;
