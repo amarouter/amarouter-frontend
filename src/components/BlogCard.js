1cac5bfa21826f241182ccc73aa5721642603989
irembuz <irembuz7@gmail.com>
2022-08-22 10:37:56 +0300

    Refactor BlogCard component. Convert it into hooks component.

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,46 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Container, Row, Col, Figure, Card } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-class BlogCard extends Component {
-  render() {
-    return (
-      <div className="blog-card mb-2">
-        <Container>
-          <Row>
-            <Col className="d-flex align-items-center" xs="4">
-              <Figure className="mb-0">
-                <Figure.Image
-                  className="mb-0"
-                  width={180}
-                  height={180}
-                  alt={this.props.card.slug}
-                  src={this.props.card.imageUrl}
-                />
-              </Figure>
-            </Col>
-            <Col xs="8">
-              <Card bg="dark" className="rounded">
-                <LinkContainer to={`/blog/${this.props.card.slug}`}>
-                  <Card.Header className="cursor-pointer">
-                    {this.props.card.title}
-                  </Card.Header>
-                </LinkContainer>
-                <Card.Body>
-                  <Card.Text as="div">{this.props.card.description}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  {this.props.card.createdAt.seconds} ·{" "}
-                  {this.props.card.readTime} min read
-                </Card.Footer>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
+const BlogCard = ({ card }) => {
+  return (
+    <div className="blog-card mb-2">
+      <Container>
+        <Row>
+          <Col className="d-flex align-items-center" xs="4">
+            <Figure className="mb-0">
+              <Figure.Image
+                className="mb-0"
+                width={180}
+                height={180}
+                alt={card.slug}
+                src={card.imageUrl}
+              />
+            </Figure>
+          </Col>
+          <Col xs="8">
+            <Card bg="dark" className="rounded">
+              <LinkContainer to={`/blog/${card.slug}`}>
+                <Card.Header className="cursor-pointer">
+                  {card.title}
+                </Card.Header>
+              </LinkContainer>
+              <Card.Body>
+                <Card.Text as="div">{card.description}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                {card.createdAt.seconds} · {card.readTime} min read
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default BlogCard;
