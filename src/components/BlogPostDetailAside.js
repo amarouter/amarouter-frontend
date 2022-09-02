490ef956ef160f8ae587cf270872285a58da189f
irembuz <irembuz7@gmail.com>
2022-09-02 14:38:08 +0300

    fix: BlogPostDetailAside navbar does not go to wanted title

--- src/components/BlogPostDetailAside.js
+++ src/components/BlogPostDetailAside.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import MarkdownNavbar from 'markdown-navbar';
+import React from "react";
+import MarkdownNavbar from "markdown-navbar";
 
 const BlogPostDetailAside = ({ blogPostSelector }) => {
-    const { blogPost } = blogPostSelector;
-    return (
-        <div className='mark-down-navbar'>
-            <MarkdownNavbar source={blogPost.text} />
-        </div>
-    )
-}
+  const { blogPost } = blogPostSelector;
+  return (
+    <div className="mark-down-navbar">
+      <MarkdownNavbar
+        source={blogPost.text}
+        headingTopOffset={window.innerWidth > 1140 ? -480 : -430}
+      />
+    </div>
+  );
+};
 
-export default BlogPostDetailAside
+export default BlogPostDetailAside;
