ccfc51cc22259bc4bb5ad8c6d8d0b673a7d593d6
irembuz <irembuz7@gmail.com>
2022-09-05 07:36:03 +0300

    code editing

--- src/components/BlogPostDetailAside.js
+++ src/components/BlogPostDetailAside.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MarkdownNavbar from "markdown-navbar";
+
 import { XLARGE_SCREEN_WIDTH_LIMIT } from "../constants/screenConstants";
 
 const BlogPostDetailAside = ({ blogPostSelector }) => {
   const { blogPost } = blogPostSelector;
+
   return (
     <div className="mark-down-navbar">
       <MarkdownNavbar
